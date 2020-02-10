54039935350347a40e61bc554e9e5d9e91a62bb7
lordrichard <pauloreizinho@hotmail.com>
2020-02-10 12:17:54 +0100

    change colors

--- src/contents/Experience.js
+++ src/contents/Experience.js
@@ -6,7 +6,8 @@ class Experience extends Component {
     render() {
         return (
             <div className='condiv'>
-                <h1 className='subtopic'>Work Experience</h1>
+                <h1 className='subtopic'>Experience</h1>
+                <Widecard title='Portefolio page' link='https://www.pauloreizinho.com/' where='Portefolio'/>
                 <Widecard title='Front Dev Intern' link='https://feinheit.ch/' where='Feinheit' from='Aug 19' to='Jan 20' />
                 {/* <Widecard title='title' where='place' from='from' to='to' /> */}
             </div>
