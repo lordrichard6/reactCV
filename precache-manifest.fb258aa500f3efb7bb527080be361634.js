c6f43ca16b0dc67f30cacaff9eef1ce8a57623f8
lordrichard <pauloreizinho@hotmail.com>
2020-02-18 14:37:01 +0100

    Updates

---
+++ precache-manifest.fb258aa500f3efb7bb527080be361634.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70dbc3ef8d81d8860ada3f48e901537",
+    "url": "/reactCV/index.html"
+  },
+  {
+    "revision": "b909f326814423431e0b",
+    "url": "/reactCV/static/css/main.77658374.chunk.css"
+  },
+  {
+    "revision": "78582b94c09337715eea",
+    "url": "/reactCV/static/js/2.595573f3.chunk.js"
+  },
+  {
+    "revision": "2bcf73e3839e96bb488d3fc4da7ab52a",
+    "url": "/reactCV/static/js/2.595573f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b909f326814423431e0b",
+    "url": "/reactCV/static/js/main.edd69fdc.chunk.js"
+  },
+  {
+    "revision": "ab372085068b32634fe4",
+    "url": "/reactCV/static/js/runtime-main.1a8846ec.js"
+  },
+  {
+    "revision": "c460a78e6d55f63afecff3f1387a20c3",
+    "url": "/reactCV/static/media/img-1.c460a78e.png"
+  },
+  {
+    "revision": "2546699933ac056a31a621c31e5e2d80",
+    "url": "/reactCV/static/media/img-2.25466999.png"
+  },
+  {
+    "revision": "69ef07d7ec353a79f559373e18c84fd0",
+    "url": "/reactCV/static/media/img-3.69ef07d7.png"
+  },
+  {
+    "revision": "6371e1ab71d98041bf294fc6fc7a43d3",
+    "url": "/reactCV/static/media/img-5.6371e1ab.png"
+  },
+  {
+    "revision": "eb05b39aa9ba805c2f0df7fced813157",
+    "url": "/reactCV/static/media/img-6.eb05b39a.png"
+  },
+  {
+    "revision": "b67188cb2c103f08216af8bb4c84583c",
+    "url": "/reactCV/static/media/me.b67188cb.jpg"
+  }
+]);
