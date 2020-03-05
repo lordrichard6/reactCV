7b96a16f2a4c4833348a8166f89b0d95374aa2a0
root <root@RICK.localdomain>
2020-03-05 09:47:10 +0100

    icon, tilt

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import profilepic from '../img/me.jpg';
 import Social from '../components/Social';
+import Tilt from 'react-tilt';
 
 
 class Home extends Component {
     render() {
         return (
             <div className='condiv home'>
-                <img src={profilepic} className='profilepic' alt="me"/>
+                <Tilt className="Tilt" options={{ max : 45 }} style={{ height: 250, width: 250 }} >
+                   <img src={profilepic} className='profilepic' alt="me"/> 
+                </Tilt>
+                
                 <ReactTypingEffect className='typingeffect' text={["My name is Paulo Reizinho", 'I am a developer', 'Welcome to my CV page', '...', 'please UX is very simple', 'click on a section on the left', '...' , 'no need to stay here all day']} speed={100} eraseDelay={700} />
                 <Social />
             </div>
--- src/contents/Projects.js
+++ src/contents/Projects.js
@@ -16,11 +16,11 @@ class Projects extends Component {
             <div className='condiv'>
                 <h1 className='subtopic'>Personal Projects</h1>
                 <ProjectCard back={img7} title='To Do' link='https://github.com/lordrichard6/svelte_todo' tool='Sveltejs' text=''/>
-                <ProjectCard back={img1} title='Face Detection App' link='https://reizinho-face-detect-app.herokuapp.com/' tool='Reactjs' text=''/>
+                <ProjectCard back={img1} title='Face Detection App' link='https://github.com/lordrichard6/faceRecognition' tool='Reactjs' text=''/>
                 <ProjectCard back={img2} title='RoboFriends' link='https://github.com/lordrichard6/robofriends' tool='React-Redux' text=''/>
-                <ProjectCard back={img3} title='Netflix' link='https://pauloreizinho-netflix.netlify.com/' tool='HTML5, CSS' text=''/>
-                <ProjectCard back={img4} title='FlappyBird' link='https://pauloreizinho-flappybird.netlify.com/' tool='Vanila JS' text=''/>
-                <ProjectCard back={img5} title='Calculator' link='https://pauloreizinho-calculator.netlify.com/' tool='Vanila JS' text=''/>
+                <ProjectCard back={img3} title='Netflix' link='https://github.com/lordrichard6/netflix_landingpage' tool='HTML5, CSS' text=''/>
+                <ProjectCard back={img4} title='FlappyBird' link='https://github.com/lordrichard6/flappyBird' tool='Vanila JS' text=''/>
+                <ProjectCard back={img5} title='Calculator' link='https://github.com/lordrichard6/calculator' tool='Vanila JS' text=''/>
                 <ProjectCard back={img6} title='Old Portfolio' link='https://pauloreizinho-portfolio2.netlify.com/' tool='HTML5, CSS' text=''/>
             </div>
         )
