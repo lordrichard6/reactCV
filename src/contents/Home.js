b9ec1e791e2e765914bad8872aede02dff900bda
lordrichard <pauloreizinho@hotmail.com>
2020-03-17 17:20:46 +0100

    nav fix, add 2 more pics

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import ReactTypingEffect from 'react-typing-effect';
-import profilepic from '../img/me.jpg';
+
 import Social from '../components/Social';
-import Tilt from 'react-tilt';
+
+import me1 from '../img/me1.jpg';
+import me2 from '../img/me2.jpg';
+import me3 from '../img/me3.jpg';
 
 
 class Home extends Component {
     render() {
         return (
             <div className='section-home col col-lg-9'>
-                <Tilt className="Tilt" options={{ max : 45 }} style={{ height: 250, width: 250 }} >
-                    <img src={profilepic} className='profilepic' alt="me"/> 
-                </Tilt> 
+                <div className="profile">
+                    <img src={me2} className='profile__photo profile__photo--2' alt="me"/> 
+                    <img src={me3} className='profile__photo profile__photo--3' alt="me"/>
+                    <img src={me1} className='profile__photo profile__photo--1' alt="me"/> 
+                </div>
+                
                 <ReactTypingEffect 
                         className='typingeffect' 
                         text={["My name is Paulo Reizinho", 
